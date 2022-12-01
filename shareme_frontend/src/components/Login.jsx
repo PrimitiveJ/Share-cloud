26260eefb5d07b0e5e0c364865149bc46ffa9706
Primitive J <hokecj@gmail.com>
2022-12-01 15:26:45 -0500

    added alt for logo

--- shareme_frontend/src/components/Login.jsx
+++ shareme_frontend/src/components/Login.jsx
@@ -19,7 +19,7 @@ const Login = () => {
       />
       <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
         <div className="p-5">
-          <img src={logo} width='130px'/>
+          <img src={logo} width='130px' alt="logo"/>
         </div>
         </div>
       </div>
